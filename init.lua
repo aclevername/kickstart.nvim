95544cc8bf1c8ee25b861d71875b6234afe4c50a
Jake <jake@syntasso.io>
2025-11-07 10:52:07 +0000

    fix warning on open

--- init.lua
+++ init.lua
@@ -658,11 +658,9 @@ require('lazy').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            vim.lsp.config(server_name, server)
+            vim.lsp.enable(server_name)
           end,
         },
       }
@@ -1100,20 +1098,15 @@ require('lazy').setup({
 })
 
 -- Golang config
-local lspconfig = require 'lspconfig'
-
-lspconfig.gopls.setup {
+vim.lsp.config('gopls', {
   cmd = { 'gopls' },
   settings = {
     gopls = {
-      analyses = {
-        unusedparams = true,
-      },
+      analyses = { unusedparams = true },
       staticcheck = true,
     },
   },
   on_attach = function(client, bufnr)
-    -- Auto-format on save
     vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = bufnr,
       callback = function()
@@ -1121,7 +1114,8 @@ lspconfig.gopls.setup {
       end,
     })
   end,
-}
+})
+vim.lsp.enable 'gopls'
 
 local dap = require 'dap'
 require('dap-go').setup()
